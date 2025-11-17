3697bdc7c211364a8f92fb1dcaadb508e9414fa7
Tomislav Vuckovic <vuckovic.tomislav1@gmail.com>
2025-11-17 16:15:22 +0100

    improve code quality standard

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,12 +1,14 @@
 import pytest
 from src.Flask_app.app import app
 
+
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
 
     with app.test_client() as client:
         yield client
+
 
 def test_hello_route(client):
     response = client.get("/")
